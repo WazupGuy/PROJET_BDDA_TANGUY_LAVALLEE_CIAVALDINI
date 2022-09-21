10af2b6e9e7199d63d79052eaf2fcb2b0c85951d
Thomas TANGUY <tanguy.thomas78500@gmail.com>
2022-09-21 17:28:55 +0200

    test du sh

---
+++ CODE/projet_test/src/projet_test/Main.java
@@ -0,0 +1,11 @@
+package projet_test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("le chemin est acquis");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test.sh
@@ -0,0 +1,2 @@
+javac -d CODE\projet_test\bin\projet_test\*.class CODE\projet_test\src\projet_test\*.java
+java -cp CODE\projet_test\bin\projet_test\*.class Main
